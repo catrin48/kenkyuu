12dba2f8969d9ba1c91579dac155637f1027c2d8
catrin48 <kussorin@gmail.com>
2025-02-28 01:11:57 +0900

    Update count.py

--- FFREM_TREM/src/count.py
+++ FFREM_TREM/src/count.py
@@ -1,21 +1,11 @@
-# ファイル名
-data_file = "swap_300.0_350.0.dat"  # 縦に0と1が記載されたファイル
+inputfile="temp_sample.out"
 
-# カウント用の変数
-count_0 = 0
-count_1 = 0
+def count1(filename):
+    with open(filename,"r") as file:
+     for value in file:
+         value=value.strip()
+         if value=="0":
 
-# ファイルを開いてデータを読み取る
-with open(data_file, "r") as file:
-    for line in file:
-        value = line.strip()  # 行末の改行を削除
-        if value == "0":
-            count_0 += 1
-        elif value == "1":
-            count_1 += 1
-total=count_0+count_1
-acceptance=count_1/total
-# 結果を出力
-print(f"0の数: {count_0}")
-print(f"1の数: {count_1}")
-print(acceptance)
+def makefilename(inputifile):
+    with open(inputifle,"r") as file:
+        filename=[line.strip() for line in file]
